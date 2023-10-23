278cf62ae3f78e91390527b8bef60dad396caaa4
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-10-23 09:24:02 +0200

    Add module scaffoldings

---
+++ R/module.R
@@ -0,0 +1,11 @@
+module_plotly <- function() {
+  # Sensible default: scatterplot + styling + tests
+}
+
+module_leaflet <- function() {
+  # Sensible default: basemaps + tests
+}
+
+module_select <- function() {
+  # Sensible default: holding options + tests
+}
